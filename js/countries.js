55d535d29a77d742f89b7c52f2d5ace72a02a36b
csmahmud99 <csmahmud.2021@gmail.com>
2024-01-17 03:07:49 +0600

    getting single country data by applying JS Array Map Method

--- js/countries.js
+++ js/countries.js
@@ -1,10 +1,19 @@
-// Fetched all country data from the External API
+// Fetch all country data from the External API
 
 const loadCountries = () => {
     fetch("https://restcountries.com/v3.1/all")
         .then(res => res.json())
         // .then(data => console.log(data));
-        .then(data => console.log(data));
+        .then(data => displayCountries(data));
 }
+
+// Display all loaded data from the External API to the Project UI
+const displayCountries = countries => {
+    // console.log(countries);
+
+    // Getting single countries data one by one by JS Array Map Method
+    const allCountriesHTML = countries.map(country => console.log(country));
+}
+
 
 loadCountries();
