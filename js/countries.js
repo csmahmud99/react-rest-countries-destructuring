28f6858865245cf1f5570379f80b048a3155405b
csmahmud99 <csmahmud.2021@gmail.com>
2024-01-17 02:36:25 +0600

    JS File is linked with the HTML file and data are fetched from the external API url

---
+++ js/countries.js
@@ -0,0 +1,10 @@
+// Fetched all country data from the External API
+
+const loadCountries = () => {
+    fetch("https://restcountries.com/v3.1/all")
+        .then(res => res.json())
+        // .then(data => console.log(data));
+        .then(data => console.log(data));
+}
+
+loadCountries();
