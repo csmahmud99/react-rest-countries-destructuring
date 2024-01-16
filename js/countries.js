af14a9c7609440b91802e0f0fde013e61d913dea
csmahmud99 <csmahmud.2021@gmail.com>
2024-01-17 03:14:49 +0600

    Basic data prepare for showing on the UI

--- js/countries.js
+++ js/countries.js
@@ -10,10 +10,24 @@ const loadCountries = () => {
 // Display all loaded data from the External API to the Project UI
 const displayCountries = countries => {
     // console.log(countries);
+    console.log(countries[0]);
 
     // Getting single countries data one by one by JS Array Map Method
-    const allCountriesHTML = countries.map(country => console.log(country));
+    // const allCountriesHTML = countries.map(country => console.log(country));
+    const allCountriesHTML = countries.map(country => getCountryHTML(country));
+
+    // console.log(allCountriesHTML);
+    // console.log(allCountriesHTML[0]);
 }
 
+// Auxiliary Function for showing the data on the UI
+const getCountryHTML = country => {
+    // console.log(country);
+    return `
+        <div>
+            <h2>${country.name.common}</h2>
+        </div>
+    `
+}
 
 loadCountries();
